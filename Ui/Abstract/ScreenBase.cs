affdb4d3063c30cf5270532d925c198e5ba18eb1
Hasan Tarhan <hmtarhan@gmail.com>
2019-05-24 16:28:44 +0300

    Update ScreenBase.cs

--- Ui/Abstract/ScreenBase.cs
+++ Ui/Abstract/ScreenBase.cs
@@ -1,6 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using Sigtrap.Relays;
+using UnityEngine.Events;
 public enum ScreenState
 {
     Showing,Hiding,Hidden,Shown
@@ -13,29 +14,28 @@ public interface IScreen
 
 public abstract class ScreenBase : MonoBehaviour,IScreen
 {
-   // [SerializeField] private Relay<bool> Showing;
-   
+    // [SerializeField] private Relay<bool> Showing;
+    public UnityEvent ShowEvent;
+    public UnityEvent HideEvent;
     private ScreenState state;
     public ScreenState State { get => state;}
+
+    private ScreenState _currentState;
+    public ScreenState CurrentState { get { return _currentState; } protected set { ScreenState oldstate = _currentState; _currentState = value; OnStateChanged(oldstate, _currentState); } }
     private void Awake()
     {
      //   Showing.AddListener(ShowHandler);
     }
     public virtual void Show()
     {
-       // Showing.Dispatch(true);
-        gameObject.SetActive(true);
-        state = ScreenState.Shown;
+        CurrentState = ScreenState.Shown;
     }
-    private void ShowHandler(bool e)
+    protected virtual void OnStateChanged(ScreenState oldState, ScreenState newState)
     {
-        //???????
+       UiManager.Instance.OnScreenStateChanged(this, oldState, newState);
     }
     public virtual void Hide()
     {
-       // Showing.Dispatch(false);
-
-        gameObject.SetActive(false);
-        state = ScreenState.Hidden;
+        CurrentState = ScreenState.Hidden;
     }
 }
